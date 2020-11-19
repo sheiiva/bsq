bceaa546d90c8c0e7b90fc97e37bf43826096837
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2020-11-19 16:41:46 +0100

    [ADD] map printing

--- sources/bsq.c
+++ sources/bsq.c
@@ -7,12 +7,25 @@
 
 #include "sources.h"
 
+int print_map(char *map)
+{
+    if (my_putstr(map, STDOUT_FILENO) == EXIT_ERROR)
+        return (EXIT_ERROR);
+    if (my_putstr("\n", STDOUT_FILENO) == EXIT_ERROR)
+        return (EXIT_ERROR);
+    return (EXIT_SUCCESS);
+}
+
 int bsq(char *filepath)
 {
     char *buffer = get_content(filepath);
 
     if (!buffer)
         return (EXIT_ERROR);
-    free(buffer);
+    // check map
+    // init int **
+    // algo
+    if (print_map(buffer) == EXIT_ERROR)
+        return (EXIT_ERROR);
     return (EXIT_SUCCESS);
 }
